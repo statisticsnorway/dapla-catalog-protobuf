c6e55af9e8f18d056833096116e396457b7d38aa
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-11-18 12:49:41 +0100

    Remove unused module dependencies

--- src/main/java-mr/11/module-info.java
+++ src/main/java-mr/11/module-info.java
@@ -1,10 +1,5 @@
 module no.ssb.dapla.catalog.protobuf {
-    requires com.google.common;
-    requires io.grpc;
-    requires grpc.stub;
     requires com.google.protobuf;
-    requires grpc.protobuf;
-    requires java.annotation;
 
     exports no.ssb.dapla.catalog.protobuf;
     opens no.ssb.dapla.catalog.protobuf;
